a718dd45466e7efbae83b92359d74f39d55ac1c7
inyangj <inyangjohnson01@gmail.com>
2023-11-25 09:45:45 +0100

    chat page UI uodate

---
+++ src/components/chatComponent/MainChat.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import menu from '../../assets/svg/menu.svg'
+import logo from '../../assets/svg/logoBlack.svg'
+
+const MainChat = () => {
+  return (
+    <section className='px-6 pt-5'>
+      <header className='flex justify-between items-center'>
+        <img src={menu} alt="menu-icon" />
+        <img src={logo} alt="logo-icon" />
+        <div></div>
+      </header>
+    </section>
+  )
+}
+
+export default MainChat
